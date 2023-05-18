9dea1b749e356210030bd654b00f0a4e070a250d
PShivers <paulsshivers@gmail.com>
2023-05-18 19:48:09 -0400

    added some basics to settings page

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -1,9 +1,18 @@
 // Step 1: Import React
 import * as React from "react";
 import Seo from "../components/seo";
+import Layout from "../components/layout";
+import { settings } from "../styles/settings.module.css";
 
 const Settings = () => {
-	return <div>Settings</div>;
+	return (
+		<Layout>
+			<div className={settings}>
+				<h1>Settings</h1>
+				<p>There are no settings yet.</p>
+			</div>
+		</Layout>
+	);
 };
 
 export const Head = () => <Seo title="Settings" />;
